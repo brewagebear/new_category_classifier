0d53546ad3356a9a22d870fa157dd6038d5eab62
dailyworker <seansin@chungbuk.ac.kr>
2019-11-25 01:43:29 +0900

    refactoring source

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -3,27 +3,17 @@ from pandas import read_table
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
-import pandas as pd
-import numpy as np
 import math
 import os.path
 
 class NaiveBayesClassifier(object):
-
     def __init__(self, k=0.5):
         self.k = k
         self.word_probs = []
-        self.word_list = []
         self.category = {"interest": 0, "jobs": 1, "moneysupply": 2, "trade": 3}
         self.word_dir = os.path.abspath('.')
         self.text_path = '/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/dev'
         self.csv_path = '/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/dev/out'
-
-    def load_corpus(self, path):
-        #말뭉치 로드를 위한 pandas read_table 함수 사용
-        corpus = read_table(path, sep=',', encoding='utf-8')
-        corpus = np.array(corpus)
-        return corpus
 
     def count_word(self, training_set):
         # 1. interest 2. jobs 3. moneysupply 4.trade
@@ -33,14 +23,8 @@ class NaiveBayesClassifier(object):
         for category, message in training_set_arr:
             for word in self.remove_stop_words(message):
                 counts[word][int(category)] += 1
+                print(counts[word])
         return counts
-
-    def isNumber(self, s):
-        try:
-            float(s)
-            return True
-        except ValueError:
-            return False
 
     def remove_stop_words(self, doc):
         tagged_list = []
@@ -56,6 +40,20 @@ class NaiveBayesClassifier(object):
         result = [t[0] for t in tagged_list if t[1] == "NN"]
         return set(result)
 
+    def train(self, training_set):
+        # 카테고리별 문서 세기
+        num_interest = len(training_set[training_set['LABLE'] == 0])
+        num_jobs = len(training_set[training_set['LABLE'] == 1])
+        num_money_supply = len(training_set[training_set['LABLE'] == 2])
+        num_trade = len(training_set[training_set['LABLE'] == 3])
+
+        # 학습 데이터 적용하여 모델 만들기
+        word_counts = self.count_word(training_set)
+        print(word_counts)
+        self.word_probs =  self.word_probabilities(word_counts, num_interest, num_jobs, num_money_supply, num_trade)
+        print(self.word_probs)
+
+
     def word_probabilities(self, counts, total_interest_new, total_jobs_news, total_money_supply_news,
                            total_trade_news):
         k = self.k
@@ -70,8 +68,7 @@ class NaiveBayesClassifier(object):
         word_probs = self.word_probs
         message_words = self.remove_stop_words(message)
         log_prob_if_interest = log_prob_if_jobs = log_prob_if_moneysupply = log_prob_if_trade = 0.0
-        print(self.word_probs)
-        print(message_words)
+
         for word, prob_if_interest, prob_if_jobs, prob_if_moneysupply, prob_if_trade in word_probs:
             if word in message_words:
                 log_prob_if_interest += math.log(prob_if_interest)
@@ -107,22 +104,3 @@ class NaiveBayesClassifier(object):
         print(max_Proba,"//",index_Proba)
         # return prob_if_spam  / (prob_if_spam + prob_if_not_spam)
         return max_Proba, index_Proba  # 제일 높은 확률과 카테고리
-
-
-    def train(self, training_set):
-        # 스팸 메시지와 햄인 메시지의 갯수 세기
-        #print(len(training_set[training_set['LABLE'] == 0]))
-        num_interest = len(training_set[training_set['LABLE'] == 0])
-        num_jobs = len(training_set[training_set['LABLE'] == 1])
-        num_money_supply = len(training_set[training_set['LABLE'] == 2])
-        num_trade = len(training_set[training_set['LABLE'] == 3])
-
-        # 학습 데이터 적용하여 모델 만들기
-        word_counts = self.count_word(training_set)
-        print(word_counts)
-        self.word_probs =  self.word_probabilities(word_counts, num_interest, num_jobs, num_money_supply, num_trade)
-        print(self.word_probs)
-
-    def classify(self, doc):
-        self.word_list = doc
-        return self.category_probability(self.word_probs, doc)
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import os
 from NaiveBayesClassifier import NaiveBayesClassifier
 
+global test_list
+
 def txt_to_csv(self):
     for dirpath, dirnames, filenames in os.walk(self.word_dir):
         for filename in filenames:
@@ -16,11 +18,20 @@ def txt_to_csv(self):
                         self.word_list.append({"category": filename.split('_')[0], "contents": clear_text})
     print(self.word_list)
 
+
 def cleanText(read_data):
     # 텍스트에 포함되어 있는 특수 문자 제거
     text = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', read_data).replace('\n', '').replace('\t', '')
     return text
 
+def get_test_file():
+    path_dir = '/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/'
+    file_list = os.listdir(path_dir)  # path 에 존재하는 파일 목록 가져오기
+    file_list.sort()  # 파일 이름 순서대로 정렬
+
+    for i in file_list:
+        f = open(path_dir+i)
+        test_list.append(f.read())
 
 if __name__ == "__main__":
     model = NaiveBayesClassifier()
@@ -31,55 +42,7 @@ if __name__ == "__main__":
                      encoding='utf-8')
     model.train(df)
     test_list = []
-
-    test1 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/1.txt', 'r').read()
-    test_list.append(test1)
-    test2 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/2.txt', 'r').read()
-    test_list.append(test2)
-    test3 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/3.txt', 'r').read()
-    test_list.append(test3)
-    test4 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/4.txt', 'r').read()
-    test_list.append(test4)
-    test5 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/5.txt', 'r').read()
-    test_list.append(test5)
-    test6 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/6.txt', 'r').read()
-    test_list.append(test6)
-    test7 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/7.txt', 'r').read()
-    test_list.append(test7)
-    test8 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/8.txt', 'r').read()
-    test_list.append(test8)
-    test9 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/9.txt', 'r').read()
-    test_list.append(test9)
-    test10 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/10.txt', 'r').read()
-    test_list.append(test10)
-    test11 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/11.txt', 'r').read()
-    test_list.append(test11)
-    test12 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/12.txt', 'r').read()
-    test_list.append(test12)
-    test13 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/13.txt', 'r').read()
-    test_list.append(test13)
-    test14 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/14.txt', 'r').read()
-    test_list.append(test14)
-    test15 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/15.txt', 'r').read()
-    test_list.append(test15)
-    test16 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/16.txt', 'r').read()
-    test_list.append(test16)
-    test17 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/17.txt', 'r').read()
-    test_list.append(test17)
-    test18 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/18.txt', 'r').read()
-    test_list.append(test18)
-    test19 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/19.txt', 'r').read()
-    test_list.append(test19)
-    test20 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/20.txt', 'r').read()
-    test_list.append(test20)
-    test21 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/21.txt', 'r').read()
-    test_list.append(test21)
-    test22 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/22.txt', 'r').read()
-    test_list.append(test22)
-    test23 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/23.txt', 'r').read()
-    test_list.append(test23)
-    test24 = open('/Users/sinsuung/Workspace/Python/unstructured_data_final_project/corpus/test/24.txt', 'r').read()
-    test_list.append(test24)
+    get_test_file()
 
     for i in test_list:
         model.category_probability(i)
